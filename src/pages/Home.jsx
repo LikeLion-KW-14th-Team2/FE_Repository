1f82e6d0e0c8e7aea1e375d32ad7b0b09774c47b
EunBin <rhdmsqls1234@naver.com>
2026-06-17 15:50:56 +0900

    [Fix] Home 배경 지도 및 깃발 위치 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,10 +16,10 @@ export function Home() {
 
     const flags = [
         //{ id: 1, top: '202px', left: '262px', name: '학사모운틴' },
-        { id: 2, top: '262px', left: '867px', name: '졸업시뮬레이션', src: Simulation_Flag, path: '/Simulation', width: '279px' },
-        { id: 3, top: '330px', left: '608px', name: '졸업요건 조회', src: Condition_Flag, path: '/Condition', width: '253px' },
-        { id: 4, top: '346px', left: '405px', name: '마이페이지', src: MyPage_Flag, path: '/MyPage', width: '187px'},
-        { id: 5, top: '172px', left: '491px', name: '가이드', src: Guide_Flag, path: '/Guide', width: '353px' }
+        { id: 2, top: '33%', left: '61%', name: '졸업시뮬레이션', src: Simulation_Flag, path: '/Simulation', width: '279px' },
+        { id: 3, top: '34.5%', left: '39%', name: '졸업요건 조회', src: Condition_Flag, path: '/Condition', width: '253px' },
+        { id: 4, top: '48%', left: '55.5%', name: '마이페이지', src: MyPage_Flag, path: '/MyPage', width: '187px'},
+        { id: 5, top: '72.5%', left: '62.5%', name: '가이드', src: Guide_Flag, path: '/Guide', width: '353px' }
     ];
 
     return (
@@ -27,28 +27,28 @@ export function Home() {
             <div className='home'>
                 <Header/>
                 
-                <img className='map-bg' src={MapImg} alt="배경 지도" 
-                    style={{ opacity: isFlagHovered ? 0.5 : 1 }}        
-                />
+                <div className='map-wrapper'>
+                    <img className={`map-bg ${isFlagHovered ? 'hovered' : ''}`} src={MapImg} alt="배경 지도" />
 
-                {/* 깃발을 클릭 가능한 버튼 태그로 생성 */}
-                {flags.map((flag) => (
-                    <button 
-                        key={flag.id}
-                        className='flag-button' 
-                        style={{
-                            top: flag.top,
-                            left: flag.left
-                        }}
-                        onClick={() => navigate(flag.path)} // 클릭 시 해당 경로로 이동
-                        aria-label={`${flag.name} 페이지로 이동`}
+                    {/* 깃발을 클릭 가능한 버튼 태그로 생성 */}
+                    {flags.map((flag) => (
+                        <button 
+                            key={flag.id}
+                            className='flag-button' 
+                            style={{
+                                top: flag.top,
+                                left: flag.left
+                            }}
+                            onClick={() => navigate(flag.path)} // 클릭 시 해당 경로로 이동
+                            aria-label={`${flag.name} 페이지로 이동`}
 
-                        onMouseEnter={() => setIsFlagHovered(true)}
-                        onMouseLeave={() => setIsFlagHovered(false)}
-                    >
-                        <img src={flag.src} alt={flag.name} style={{width:flag.width}}/>
-                    </button>
-                ))}
+                            onMouseEnter={() => setIsFlagHovered(true)}
+                            onMouseLeave={() => setIsFlagHovered(false)}
+                        >
+                            <img src={flag.src} alt={flag.name} style={{width:flag.width}}/>
+                        </button>
+                    ))}   
+                </div>
             </div>
         </>
     )
